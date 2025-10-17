415fea1609241519df3ad667c0df3cbf9eeaef71
Bhuvana6590 <ayyanarthunai6590@gmail.com>
2025-10-17 12:54:31 +0530

    random module
    random module

---
+++ random module.py
@@ -0,0 +1,21 @@
+#random module
+'''
+import random
+ran=random.random()
+print(ran)
+'''
+'''
+import random
+random.seed(100)
+ran=random.random()
+print(ran)
+'''
+
+#randrange
+from random import*
+print(randrange(1,92))
+
+#randint
+#specified range
+print(randint(1,5))
+print(uniform(20,60))
