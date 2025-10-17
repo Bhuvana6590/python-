14ada34935734a23cb16e0227b8153a6978b6032
Bhuvana6590 <ayyanarthunai6590@gmail.com>
2025-10-17 12:55:53 +0530

    Relational operators
    Relational operators

---
+++ relational operator.py
@@ -0,0 +1,72 @@
+'''
+x=int(input('enter the number 1='))
+y=int(input('enter the number 2='))
+print('equal is=',x==y)
+print('equal is=',x!=y)
+print('equal is=',x>y)
+print('equal is=',x<y
+print('equal is=',x>=y)
+print('equal is=',x<=y)
+'''
+
+'''
+# logical operatorx=int(input('enter the number 1='))
+x=int(input('enter the number 1='))
+y=int(input('enter the number 2='))
+print(x==y and x!=y)
+print(x==y or x!=y)
+'''
+#Membership operators
+names=['venkat','prasanth','bhuvana']
+print('venkat'in names)
+print('satish' in names)
+print('Lokesh'not in names)
+
+'''
+'''
+#identity operator
+x=90
+y=90
+print(id(x))
+print(id(y))
+print(x is y)
+print(x is not y)
+'''
+'''
+# identity operator in list
+l1=[1,2,3]
+l2=[1,2,3]
+print(id(l1))
+print(id(l2))
+print(l1 is l2)
+print(l1 is not l2)
+'''
+'''
+#bitwise operator
+print(bin(45))
+print(oct(25))
+print(oct(145))
+print(hex(47))
+print(hex(56))
+
+x=2
+y=5
+print('AND value is=',x&y)
+print('OR value is=',x|y)
+print('AND value is=',x^y)
+
+x=3
+print('not value is=',~x)
+y=-8
+print('not value is=',~y)
+
+'''
+
+'''
+
+
+
+
+
+
+      
