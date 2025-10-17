c294240ae2be33202b15ed12dbe55f9085de7ffb
Bhuvana6590 <ayyanarthunai6590@gmail.com>
2025-10-17 12:22:38 +0530

    Operators
    Python assignment operators

---
+++ assignment operators.py
@@ -0,0 +1,22 @@
+x=int(input('enter the no1='))
+y=int(input('enter the no2='))
+z=x+y
+print('value of z is',z)
+#z=Z+x
+z+=x
+print('addition value of z is',z)
+z-=y
+print('subtraction value of z is',z)
+z*=y
+print('multilication value of z is',z)
+z/=y
+print('division value of z is',z)
+z%=y
+print('remainder value of z is',z)
+z//=y
+print('floor division value of z is',z)
+z**=y
+print('power value of z is',z)
+
+
+
