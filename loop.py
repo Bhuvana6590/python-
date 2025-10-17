5544bf3756987d6f13e9dd47915b59e07e18d473
Bhuvana6590 <ayyanarthunai6590@gmail.com>
2025-10-17 12:50:07 +0530

    Loop
    Loop

---
+++ loop.py
@@ -0,0 +1,38 @@
+# for loop
+'''
+#1
+x=int(input('enter number'))
+for i in range(x):
+print(i)
+
+#2 
+for i in range(1,11):
+    print(i)
+'''
+'''
+# odd
+for i in range(1,10,2):
+    print(i)
+'''
+
+
+#3 even
+for i in range(2,10,2):
+    print(i)
+
+#4 negative number
+
+for i in range(-11,0):
+    print(i)
+    
+#5
+x=[1,2,3,4,5,6,7]
+for i in x:
+    if i%2==0:
+        print(i)
+    
+
+
+
+
+
