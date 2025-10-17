325254d3c317103ca123995c66e2c2266e147861
Bhuvana6590 <ayyanarthunai6590@gmail.com>
2025-10-17 12:20:33 +0530

    python assignment
    python assignment

---
+++ Assignment 1.py
@@ -0,0 +1,44 @@
+'''
+list1=[2,33,222,14,25]
+print(list1)
+print(list1[:-1])
+print(list1)
+
+
+names=['amir','bear','charlton','damn']
+print(names[-1][-1])
+
+list1=[1,3]
+list2 = list1
+list1[0]=4
+print(list2)
+
+
+
+a=input(" enter the number:")
+print (a)
+
+b=int(input("enter number:"))
+print(b)      
+
+
+
+
+l=int(input(" enter the number:"))
+m=int(input(" enter the number:"))
+print("Addition :",l+m )
+print("subtraction :",l-m )
+print("multiplication :",l*m )
+print("division :",l/m )
+print("flow division :",l//m )
+print("percentage :",l%m )
+print("exponent :",l**m )
+
+'''
+
+l=int(input(" enter the number:"))
+m=int(input(" enter the number:"))
+l**=m
+print(l)
+
+
