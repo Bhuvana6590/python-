2f01f07005646901f266059ad429767004ab42d3
Bhuvana6590 <ayyanarthunai6590@gmail.com>
2025-10-17 12:41:39 +0530

    Jumping statement
    Jumping statement

---
+++ jumping statement.py
@@ -0,0 +1,19 @@
+'''#break
+for i in range(0,10):
+    if i==5:
+        break
+    print(i)
+    
+#continue
+for i in range(0,10):
+    if i%2==0:
+        continue
+    print(i)
+'''
+#pass
+for i in range(0,10):
+    if i==5:
+        pass
+    print(i)
+    
+    
